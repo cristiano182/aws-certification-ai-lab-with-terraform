0c72d1effcdb65c1512e5dcc99cb24077ff6ccaa
Cristiano Oliveira <cristiano.osantos@stone.com.br>
2026-06-30 18:54:35 -0300

    ok

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-1746926346063"
+    bucket         = "terraform-state-bucket-dev-1746926346063"
     dynamodb_table = "terraform-state-table-lock"
     encrypt        = true
   }
